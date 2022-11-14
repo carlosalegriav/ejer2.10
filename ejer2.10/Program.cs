c56d40e344be479443bda4c787bc911b91a7e998
Lenovo <Lenovo@DESKTOP-620RG2R>
2022-11-13 22:02:18 -0500

    Agregar archivos de proyecto.

---
+++ ejer2.10/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ejerciciodosdiez
+{
+    public class program
+    {
+        public static void Main()
+        {
+            double pulg = 0;
+            double Mt = 0.0254;
+            double Cpulg = 0;
+            double MeDados = 0;
+            Console.WriteLine("Dijite los metros necesitados");
+            MeDados = double.Parse(Console.ReadLine());
+            Cpulg = MeDados / Mt;
+
+            Console.WriteLine($"La cantidad de pulgadas que se debe pedir es :{Cpulg}");
+
+        }
+    }
+}
